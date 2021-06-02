d2310214a91db19b0a19163ca12867bd7dc28824
Konradrundfunk <33392939+Konradrundfunk@users.noreply.github.com>
2021-06-02 21:33:38 +0200

    desktop os support

--- lib/constants.dart
+++ lib/constants.dart
@@ -2,4 +2,4 @@ import 'package:flutter/material.dart';
 
 const primaryswatch = Color(0xFF0F5E98);
 final double std_padding = 15;
-final admin_mail = "konrad@eichsfeld.net";
+final admin_mail = "mail@mail.de";
