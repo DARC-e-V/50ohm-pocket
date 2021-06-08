e1a7309dc08f2af56c2abe0dfb7191f4c4b9f597
konradrundfunk <konrad@eichsfeld.net>
2021-06-08 15:10:54 +0200

    cleaned up code

--- lib/navbar.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter/material.dart';
-
-bottomnavbar() => BottomNavigationBar(
-    items: const <BottomNavigationBarItem>[
-    BottomNavigationBarItem(icon: Icon(Icons.fact_check_outlined), title: Text("Lernen")),
-    BottomNavigationBarItem(icon: Icon(Icons.school_outlined), title: Text("Prüfungssimulation")),
-    BottomNavigationBarItem(icon: Icon(Icons.switch_account_outlined), title: Text("Lernstand")),
-  ],
-);
