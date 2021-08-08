397aace7a04ad640bef697541064799aa67dbd16
konradrundfunk <konrad@eichsfeld.net>
2021-08-08 18:03:18 +0200

    deeper integration of db

--- lib/screens/learning/finish.dart
+++ lib/screens/learning/finish.dart
@@ -4,23 +4,24 @@ import 'package:flutter/widgets.dart';
 
 
 class Finish extends StatefulWidget{
-  var mainchapter, subchapter, chapter, question;
-  Finish(this.mainchapter, this.subchapter, this.chapter, this.question);
+  var mainchapter, subchapter, chapter, result;
+  Finish(this.mainchapter, this.chapter, this.subchapter, this.result);
 
   @override
   State<StatefulWidget> createState() =>
-     _finishstate(mainchapter, subchapter, chapter, question);
+     _finishstate(mainchapter, subchapter, chapter, result);
 
 }
 
 class _finishstate extends State<Finish>{
-  var mainchapter, subchapter, chapter, question;
+  var mainchapter, subchapter, chapter, result;
 
-  _finishstate(this.mainchapter, this.subchapter, this.chapter, this.question);
+  _finishstate(this.mainchapter, this.subchapter, this.chapter, this.result);
   
   @override
   Widget build(BuildContext context) {
-    Databaseobj(context, 0, 0, 0, 0).write();
+    
+    Databaseobj(context, mainchapter, chapter, subchapter, result).write();
     return Scaffold(
       body: Column(
         children: [
