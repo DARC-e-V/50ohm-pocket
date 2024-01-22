405acb712e2ac2a94a2975cddf276c8014d0cad7
Felix Pfannkuch <do6fp@darc.de>
2024-01-22 22:33:24 +0100

    made it buildable for android - changed Class names from amateurfunktrainer to fuenfzigohm - edited build.gradle

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:amateurfunktrainer/main.dart';
+import 'package:fuenfzigohm/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
