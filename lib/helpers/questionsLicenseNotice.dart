1918a4f19550f3264324913c2530bb302fecd4d0
Felix Pfannkuch <do6fp@darc.de>
2024-01-28 22:15:27 +0100

    improved exam questionsLicenseNotice.dart

--- lib/helpers/questionsLicenseNotice.dart
+++ lib/helpers/questionsLicenseNotice.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:url_launcher/url_launcher.dart';
+import 'package:settings_ui/settings_ui.dart';
 
 class QuestionsLicensePage extends StatelessWidget {
   _launchURL(String url) async {
@@ -16,43 +17,39 @@ class QuestionsLicensePage extends StatelessWidget {
       appBar: AppBar(
         title: Text('Amateurfunkprüfungsbescheinigungen'),
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Text(
-              'Prüfungsfragen zum Erwerb von Amateurfunkprüfungsbescheinigungen,',
-              style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
-            ),
-            SizedBox(height: 8),
-            Text(
-              'Bundesnetzagentur, 2. Auflage, Dezember 2023,',
-              style: TextStyle(fontSize: 16),
-            ),
-            SizedBox(height: 8),
-            GestureDetector(
-              onTap: () => _launchURL('https://www.bundesnetzagentur.de/amateurfunk'),
-              child: Text(
-                'www.bundesnetzagentur.de/amateurfunk',
-                style: TextStyle(fontSize: 16, color: Colors.blue),
+      body: SettingsList(
+        sections: [
+          SettingsSection(
+            title: Text('Prüfungsfragen zum Erwerb von Amateurfunkprüfungsbescheinigungen', style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold)),
+            tiles: [
+              SettingsTile(
+                title: Text('Bundesnetzagentur, 2. Auflage, Dezember 2023', style: TextStyle(fontSize: 16)),
+                description: GestureDetector(
+                  onTap: () => _launchURL('https://www.bundesnetzagentur.de/amateurfunk'),
+                  child: Text(
+                    'www.bundesnetzagentur.de/amateurfunk',
+                    style: TextStyle(fontSize: 16, color: Colors.blue),
+                  ),
+                ),
               ),
-            ),
-            SizedBox(height: 16),
-            Text(
-              'Datenlizenz Deutschland – Namensnennung – Version 2.0',
-              style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
-            ),
-            SizedBox(height: 8),
-            GestureDetector(
-              onTap: () => _launchURL('https://www.govdata.de/dl-de/by-2-0'),
-              child: Text(
-                'www.govdata.de/dl-de/by-2-0',
-                style: TextStyle(fontSize: 16, color: Colors.blue),
+            ],
+          ),
+          SettingsSection(
+            title: Text('Datenlizenz Deutschland – Namensnennung – Version 2.0', style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold)),
+            tiles: [
+              SettingsTile(
+                title: Text(""),
+                description: GestureDetector(
+                  onTap: () => _launchURL('https://www.govdata.de/dl-de/by-2-0'),
+                  child: Text(
+                    'www.govdata.de/dl-de/by-2-0',
+                    style: TextStyle(fontSize: 16, color: Colors.blue),
+                  ),
+                ),
               ),
-            ),
-          ],
-        ),
+            ],
+          ),
+        ],
       ),
     );
   }
