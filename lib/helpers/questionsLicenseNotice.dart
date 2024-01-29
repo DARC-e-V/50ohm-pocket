af25b4ff95db7a545a9ec7e015b2059c15075205
Felix Pfannkuch <do6fp@darc.de>
2024-01-29 20:55:57 +0100

    final touches based on discussion #24

--- lib/helpers/questionsLicenseNotice.dart
+++ lib/helpers/questionsLicenseNotice.dart
@@ -15,36 +15,25 @@ class QuestionsLicensePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Amateurfunkprüfungsbescheinigungen'),
+        title: Text('Lizenzhinweise'),
       ),
       body: SettingsList(
         sections: [
           SettingsSection(
-            title: Text('Prüfungsfragen zum Erwerb von Amateurfunkprüfungsbescheinigungen', style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold)),
+            title: Text(''),
             tiles: [
               SettingsTile(
-                title: Text('Bundesnetzagentur, 2. Auflage, Dezember 2023', style: TextStyle(fontSize: 16)),
+                title: Text('Bundesnetzagentur,\n 2. Auflage, Dezember 2023 \n'),
                 description: GestureDetector(
                   onTap: () => _launchURL('https://www.bundesnetzagentur.de/amateurfunk'),
-                  child: Text(
-                    'www.bundesnetzagentur.de/amateurfunk',
-                    style: TextStyle(fontSize: 16, color: Colors.blue),
-                  ),
+                  child: Text('www.bundesnetzagentur.de/amateurfunk \n \n Prüfungsfragen zum Erwerb von Amateurfunkprüfungsbescheinigungen \n \n Änderungen: HTML Tags wurden aus den Fragen entfernt', style: TextStyle(fontStyle: FontStyle.italic)),
                 ),
               ),
-            ],
-          ),
-          SettingsSection(
-            title: Text('Datenlizenz Deutschland – Namensnennung – Version 2.0', style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold)),
-            tiles: [
               SettingsTile(
                 title: Text(""),
                 description: GestureDetector(
                   onTap: () => _launchURL('https://www.govdata.de/dl-de/by-2-0'),
-                  child: Text(
-                    'www.govdata.de/dl-de/by-2-0',
-                    style: TextStyle(fontSize: 16, color: Colors.blue),
-                  ),
+                  child: Text('Datenlizenz Deutschland – Namensnennung – Version 2.0 \n www.govdata.de/dl-de/by-2-0'),
                 ),
               ),
             ],
