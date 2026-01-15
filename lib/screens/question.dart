DARC-e-V/50ohm-pocket

commit bcf7dc641e0d76ca6dafad2a7de3102a899426fb

feat: render <u> with underscore

lib/screens/question.dart
@@ -462,6 +462,8 @@ List<InlineSpan> parseHtml(String htmlString, TextStyle style) {
          newStyle = style.copyWith(fontWeight: FontWeight.bold);
        } else if (node.localName == 'i' || node.localName == 'em') {
          newStyle = style.copyWith(fontStyle: FontStyle.italic);
+       } else if (node.localName == 'u' || node.localName == 'ins') {
+         newStyle = style.copyWith(decoration: TextDecoration.underline);
        } else if (node.localName == 'br') {
           spans.add(TextSpan(text: "\n", style: style));
           continue;

commit a5bdb21d8813b0eeb1df1a31d320e93472bac72d

fix: wrong text color introduced in bcf7dc641e0d76ca6dafad2a7de3102a899426fb

lib/screens/question.dart
@@ -150,17 +150,17 @@ class _Questionstate extends State<Question> with TickerProviderStateMixin {
                   Padding(
                     padding: EdgeInsets.only(top: std_padding, left: std_padding, right: std_padding),
                     child: Center(
-                      child: RichText(
-                        textAlign: TextAlign.left,
-                        text: TextSpan(
+                      child: Text.rich(
+                        TextSpan(
                             children: parseTextWithMath(
                               "${json.questionname(chapter,subchapter.length == 0 ? Null : subchapter[subchapterkey], questionorder[questionkey])}",
                               TextStyle(
                                   fontWeight: FontWeight.w400,
-                                  fontSize: 22
+                                  fontSize: 22,
                               ),
                             )
                         ),
+                        textAlign: TextAlign.left,
                       ),
                     ),
                   ),
@@ -307,17 +307,17 @@ class _Questionstate extends State<Question> with TickerProviderStateMixin {
                           });
                         }
                       },
-                      title: RichText(
-                        textAlign: TextAlign.left,
-                        text: TextSpan(
+                      title: Text.rich(
+                        TextSpan(
                             children: parseTextWithMath(
                               "${ShuffledAnswers[i]}",
                               TextStyle(
                                   fontWeight: FontWeight.w400,
-                                  fontSize: 22
+                                  fontSize: 22,
                               ),
                             )
                         ),
+                        textAlign: TextAlign.left,
                       )
                   ),
                 ),