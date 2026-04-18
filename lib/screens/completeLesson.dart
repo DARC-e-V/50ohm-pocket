eea83592d47f2449094347b6621eb305b7cb5e58
D45Hub <denis.thiessen@gmx.de>
2026-04-18 18:36:57 +0200

    Remove unsued imports

--- lib/screens/completeLesson.dart
+++ lib/screens/completeLesson.dart
@@ -1,5 +1,3 @@
-import 'package:fuenfzigohm/custom_libs/database.dart';
-import 'package:fuenfzigohm/custom_libs/json.dart';
 import 'package:fuenfzigohm/style/style.dart';
 import 'package:fl_chart/fl_chart.dart';
 import 'package:flutter/material.dart';
