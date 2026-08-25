688199d132fe4edc3c1df28b50059d1047c6f27f
Matthias Jung <m.jung@uni-wuerzburg.de>
2026-08-26 00:30:54 +0200

    fix: styling

--- lib/style/style.dart
+++ lib/style/style.dart
@@ -3,11 +3,34 @@ import 'package:flutter/material.dart';
 // TODO: Set up all colors, warning and success
 lightmode() => ThemeData.from(
       colorScheme: ColorScheme.light(primary: Color(0xFF01ACF1)),
+    ).copyWith(
+      appBarTheme: const AppBarTheme(
+        surfaceTintColor: Colors.transparent,
+        scrolledUnderElevation: 0,
+      ),
+      progressIndicatorTheme: const ProgressIndicatorThemeData(
+        linearTrackColor: Color(0xFFE0E0E0),
+      ),
+      popupMenuTheme: const PopupMenuThemeData(
+        surfaceTintColor: Colors.transparent,
+      ),
     );
 
 // TODO: Set up all colors, warning and success
 darkmode() => ThemeData.from(
       colorScheme: ColorScheme.dark(primary: Color(0xFF01ACF1)),
+    ).copyWith(
+      appBarTheme: const AppBarTheme(
+        surfaceTintColor: Colors.transparent,
+        scrolledUnderElevation: 0,
+      ),
+      progressIndicatorTheme: const ProgressIndicatorThemeData(
+        linearTrackColor: Color(0xFF49454F),
+      ),
+      popupMenuTheme: const PopupMenuThemeData(
+        color: Color(0xFF29262F),
+        surfaceTintColor: Colors.transparent,
+      ),
     );
 
 // TODO: Remove this, discuss general button style, derivates, etc.
